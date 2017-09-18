0cb29e3ce10c2423657fe8f3d97c89b5e0b999a6
wangcan26 <15002274795@163.com>
2017-09-18 12:02:41 +0800

    [source]add multiple demos

--- 2.RenderWindow/main.cc
+++ 2.RenderWindow/main.cc
@@ -1,1 +1,27 @@
 //RenderWindow demo
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "The title");
+    
+    while(window.isOpen())
+    {
+        //Handle events
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        //Update scene
+        
+        //Render cycle
+        window.clear(sf::Color::Red);
+        
+        //Render objects
+        window.display();
+    }
+    return 0;
+}
